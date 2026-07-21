7db8c510d06e06c078851a959675d53f60227174
jorge <jorgepb2007@gmail.com>
2026-07-21 08:48:03 -0500

    Update assembly informational version and cache inputs

--- obj/Debug/net8.0/curso.AssemblyInfo.cs
+++ obj/Debug/net8.0/curso.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("curso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02232699f53fc05792f3e12f88a1193941e597cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a031aba3352fc73d256141bd4417cd93351be1")]
 [assembly: System.Reflection.AssemblyProductAttribute("curso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("curso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
